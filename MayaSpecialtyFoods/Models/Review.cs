b7bec425bab3fc11274ba02700760bae4023302f
Kim Schulze <kimberlykayschulze@gmail.com>
2017-10-25 22:03:36 -0700

    Pass review model tests.

--- MayaSpecialtyFoods/Models/Review.cs
+++ MayaSpecialtyFoods/Models/Review.cs
@@ -10,27 +10,42 @@ namespace MayaSpecialtyFoods.Models
         [Key]
         public int ReviewId { get; set; }
         public string Author { get; set; }
-        public string Content_Body { get; set; }
+        public string ContentBody { get; set; }
         public int Rating { get; set; }
         public int ProductId { get; set; }
         public virtual Product Product { get; set; }
 
-        public override bool Equals(System.Object otherProduct)
+        public Review(int reviewid, string author, string contentbody, int rating, int productid)
         {
-            if (!(otherProduct is Product))
+            ReviewId = reviewid;
+            Author = author;
+            ContentBody = contentbody;
+            Rating = rating;
+            ProductId = productid;
+        }
+
+        public override bool Equals(System.Object otherReview)
+        {
+            if (!(otherReview is Review))
             {
                 return false;
             }
             else
             {
-                Product newProduct = (Product)otherProduct;
-                return this.ProductId.Equals(newProduct.ProductId);
+                Review newReview = (Review)otherReview;
+                bool idEquality = this.ReviewId == newReview.ReviewId;
+                bool authorEquality = this.Author == newReview.Author;
+                bool contentBodyEquality = this.ContentBody == newReview.ContentBody;
+                bool ratingEquality = this.Rating == newReview.Rating;
+                bool productIdEquality = this.ProductId == newReview.ProductId;
+
+                return (idEquality && authorEquality && contentBodyEquality && ratingEquality && productIdEquality);
             }
         }
 
         public override int GetHashCode()
         {
-            return this.ProductId.GetHashCode();
+            return this.ReviewId.GetHashCode();
         }
     }
 }
---
+++ MayasSpecialtyFoods.Tests/ModelTests/ReviewsTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MayaSpecialtyFoods.Models;
+using MayaSpecialtyFoods.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MayaSpecialtyFoods.Tests
+{
+    [TestClass]
+    public class ReviewsTest
+    {
+        MayaSpecialtyFoodsContext db = new MayaSpecialtyFoodsContext();
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<MayaSpecialtyFoodsContext>(options => options.UseMySql(@"Server=localhost;Port=3306;database=mayaspecialtyfoodstest;uid=root;pwd=root;"));
+        }
+
+        [TestMethod]
+        public void Equals_OverrideTrueForSameReviewProperties_Review()
+        {
+            //Arrange, Act
+            Review firstReview = new Review(1, "Maya", "Loved it!", 5, 1);
+            Review secondReview = new Review(1, "Maya", "Loved it!", 5, 1);
+
+            //Assert
+            Assert.AreEqual(firstReview, secondReview);
+        }
+
+        [TestMethod]
+        public void Equals_ReviewNameIsFoundInReviewObject_Name()
+        {
+            //Arrange
+            Review newReview = new Review(1, "Maya", "Loved it!", 5, 1);
+            string expected = "Maya";
+
+            //Act
+            string result = newReview.Author;
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+
+}
